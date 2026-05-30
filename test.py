18a6dc17ff414aa839e8613d9fd0a2ca4f75e025
Shaik Afnan <shaikafnan095@gmail.com>
2026-05-30 22:48:45 +0530

    test: tested activation functions

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from vectors import dot
+from activation import *
 
 inputs = [1,2]
 weights = [0.5,0.3]
@@ -6,4 +7,7 @@ bias = 0.1
 
 output = dot(inputs,weights) + bias
 
-print(output)
+print(sigmoid(output))
+print(relu(output))
+print(sigmoid_derivative(output))
+print(relu_derivative(output))
