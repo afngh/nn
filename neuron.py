540a0ff2f4c36748e15fb97e1c977999f3cf83f6
Shaik Afnan <shaikafnan095@gmail.com>
2026-05-31 12:25:59 +0530

    add: implemented neural networks with the help of neuron&neuralLayer(classes)

--- neuron.py
+++ neuron.py
@@ -12,35 +12,53 @@ class Neuron():
         self.a_type = a_type
 
     def forward(self,input):
+        print(f"inputs: {input}")
+        print(f"weights: {self.weights}")
         self.z = dot(input,self.weights) + self.bias
         return self.a_type(self.z)
 
-    def learn(self,inputs,y_pred,y_true,learning_rate=0.1):
-        gradient = compute_gradients(inputs,y_pred,y_true,self.z)
-        self.weights = gradient_descent(self.weights,gradient,learning_rate)
+    # def learn(self,inputs,y_pred,y_true,learning_rate=0.1):
+    #     gradient = compute_gradients(inputs,y_pred,y_true,self.z)
+    #     self.weights = gradient_descent(self.weights,gradient,learning_rate)
 
 class NeuralLayer():
     def __init__(self,num_neurons,layer_size,a_type=sigmoid):
         self.num_neurons = num_neurons
         self.layer_size = layer_size
-        self.a_type = a_type
-        self.neurons = [Neuron([random.uniform(0,1),random.uniform(0,1)],0.01,a_type) for _ in range(num_neurons)]
-        for neuron in self.neurons:
-            print(neuron.weights)
+        self.neurons = [Neuron(weights=list(.5 for _ in range(layer_size)),bias=random.uniform(0,.5),a_type=a_type) for _ in range(num_neurons)]
 
     def forward(self,input):
+        print(f"length of nl: {self.neurons}")
         return [neuron.forward(input) for neuron in self.neurons]
 
-    def learn(self,inputs,y_true,learning_rate=0.01):
-        for neuron in self.neurons:
-            y_pred = neuron.forward(inputs)
-            while(mse(y_pred,y_true) > 0.01):   
-                gradient = compute_gradients(inputs,y_pred,y_true,neuron.z)
-                print(f"gradients: {gradient}")
-                neuron.weights = gradient_descent(neuron.weights,gradient,learning_rate)
-                y_pred = neuron.forward(inputs)
-                print(f"weights: {neuron.weights}")
-                # sleep(.3)
+    # def learn(self,inputs,y_true,learning_rate=0.01):
+    #     for neuron,idx in zip(self.neurons,range(self.num_neurons)):
+    #         y_pred = neuron.forward(inputs)
+    #         while(mse(y_pred,y_true) > 0.01):   
+    #             gradient = compute_gradients(inputs,y_pred,y_true,neuron.z)
+    #             neuron.weights = gradient_descent(neuron.weights,gradient,learning_rate)
+    #             y_pred = neuron.forward(inputs)
+    #         print(f'Trained Neuron: {idx}')
         
-        for neuron in self.neurons:
-            print(neuron.weights)
+    #     for neuron in self.neurons:
+    #         print(neuron.weights)
+
+
+
+
+class NeuralNetwork():
+    def __init__(self):
+        self.layers = []
+
+    def add(self, layer):
+        self.layers.append(layer)
+        print("Added Layer")
+
+    def forward(self, inputs):
+        output = inputs
+        print("Init NN")
+        for layer in self.layers:
+            output = layer.forward(output)
+            print(f"Output of NN Layer: {output}")
+
+        return output
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from neuron import NeuralNetwork
 from loss import compute_gradients
 from vectors import dot
 from activation import *
@@ -6,13 +7,13 @@ from neuron import Neuron
 from loss import *
 from neuron import NeuralLayer
 
-inputs = [5,5]
+inputs = [2,3,5]
 
-y_true = 15
+nn = NeuralNetwork()
 
-ln = NeuralLayer(num_neurons=3,layer_size=2,a_type=relu)
+nn.add(NeuralLayer(num_neurons=3,layer_size=3,a_type=relu))
+nn.add(NeuralLayer(num_neurons=5,layer_size=3,a_type=relu))
+nn.add(NeuralLayer(num_neurons=1,layer_size=5,a_type=relu))
 
-ln.learn(inputs,y_true,learning_rate=0.1)
-
-for neuron in ln.neurons:
-    print(f"pred: {neuron.forward([100,200])}")
+out = nn.forward(inputs=inputs)
+print(out)
